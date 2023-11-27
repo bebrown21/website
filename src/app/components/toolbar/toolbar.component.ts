9816e24c8af5e6d6a7e93a2b1b1e67844558e449
Brandon Brown <brandonbrownb2@gmail.com>
2023-11-27 11:18:39 -0600

    setup base components and routes

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: 'toolbar.component.html',
+  styleUrls: ['toolbar.component.scss'],
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
 })
 export class ToolbarComponent {}
