761832abff2e504d37ec7ec16047d67742aea13c
Brandon Brown <brandonbrownb2@gmail.com>
2025-01-20 09:33:08 -0600

    add theming

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,19 +6,20 @@ import { RouterModule } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 import { Option, ThemeService } from 'src/app/services/theme.service';
 import { CommonModule } from '@angular/common';
+import { ThemeManager } from 'src/app/services/theme-manager.service';
 
 @Component({
-    selector: 'app-toolbar',
-    templateUrl: 'toolbar.component.html',
-    styleUrls: ['toolbar.component.scss'],
-    imports: [
-        CommonModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatButtonModule,
-        RouterModule,
-        MatMenuModule,
-    ]
+  selector: 'app-toolbar',
+  templateUrl: 'toolbar.component.html',
+  styleUrls: ['toolbar.component.scss'],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+    MatMenuModule,
+  ],
 })
 export class ToolbarComponent implements OnInit {
   public options: Option[] = [
@@ -51,6 +52,13 @@ export class ToolbarComponent implements OnInit {
       value: 'purple-green',
     },
   ];
+
+  themeManager = inject(ThemeManager);
+  isDark$ = this.themeManager.isDark$;
+
+  changeTheme(theme: string) {
+    this.themeManager.changeTheme(theme);
+  }
 
   ngOnInit() {
     // this.themeService.setTheme(this.options[0].value);
